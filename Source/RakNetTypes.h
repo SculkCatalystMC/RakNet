3dfa7bbf05e41e75abdb146708b7e8d2021f6b12
Fluorine <186297309+DivanadiumPentaoxide@users.noreply.github.com>
2026-06-13 15:55:45 +0800

    feat: std::hash support for RakNetGUID

--- Source/RakNetTypes.h
+++ Source/RakNetTypes.h
@@ -572,3 +572,9 @@ struct RAK_DLL_EXPORT uint24_t {
 } // namespace RakNet
 
 #endif
+
+
+template <>
+struct std::hash<RakNet::RakNetGUID> {
+    size_t operator()(const RakNet::RakNetGUID& v) const { return static_cast<size_t>(v.g); }
+};
